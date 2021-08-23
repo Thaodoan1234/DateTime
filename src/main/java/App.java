c77fc2935f2090f5f3d0471202a78aebb56ce3ec
Thaodoan1234 <thao.doan1@onemount.com>
2021-08-23 21:37:44 +0700

    update commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -24,41 +24,31 @@ public class App {
         int year = sc.nextInt();
         System.out.print("\nEnter month: ");
         int month = sc.nextInt();
-        int day, second;
+        int day;
 
-        switch (month) {
-            case 1:
-            case 3:
-            case 5:
-            case 7:
-            case 8:
-            case 10:
-            case 12:
-                day = 31;
-                System.out.format("Tháng có %d giây ", 31*3600);
-            case 4:
-            case 6:
-            case 9:
-            case 11:
-                day = 30;
-                System.out.format("Tháng có %d giây ", 30*3600);
-            case 2:
-                if ((year % 400 == 0) || (year % 4 == 0) && (year % 100 != 0)) {
-                    day = 29;
-                    System.out.format("Tháng có %d giây ", 29*3600);
-                } else {
-                    day = 28;
-                    System.out.format("Tháng có %d giây ", 28*3600);
-                }
-            default:
-                   System.out.println("bạn nhập không đúng");
+        if(month==1 || month==3 || month==5 || month==7 || month==8 || month==10 || month==12) {
+            System.out.format("Tháng %d có %d giây", month, 31*3600);
+        }
+        else if(month==4 || month==6 || month==9 || month==11) {
+            System.out.format("Tháng %d có %d giây", month, 30*3600);
+        }
+        else if(month==2) {
+            if((year % 400 == 0) || (year % 4 == 0) && (year % 100 != 0)) {
+                System.out.format("Tháng %d có %d giây", month, 29*3600);
+            }
+            else {
+                System.out.format("Tháng %d có %d giây", month, 28*36000);
+            }
+        }
+        else {
+            System.out.println("Tháng không hợp lệ");
         }
     }
 
     //lấy số giây của năm bất kỳ
     public static void secondOfYear() {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter year: ");
+        System.out.print("\nEnter year: ");
         int year = sc.nextInt();
         if((year % 400 == 0) || (year % 4 == 0) && (year % 100 != 0)) {
             System.out.format("Số giây của năm %d là: %d",year, 365*3600);
